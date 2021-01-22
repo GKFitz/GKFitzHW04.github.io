60545c3846ef54b7e9d5a91a98ae085368676d42
gkf <gkf2103@gmail.com>
2021-01-21 19:35:03 -0500

    basic outline

---
+++ script.js
@@ -0,0 +1,23 @@
+// Ask the Questions. Create the arrays for each individual answer to the questions
+// record the answers
+// record the answers
+
+// gather answer containers from our quiz
+
+
+// keep track of user's answers for each question...
+
+
+// find selected answer
+  
+// if answer is correct, gain time on timer
+   
+// Bonus: color the correct answers green
+  
+// if answer is wrong or blank, lose time on the timer
+
+// Bonus: color the incorrect answers red
+
+// Show users score
+
+//  record user score and initials: local storage
