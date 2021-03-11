ec52b7cccf7e4b02de849652e551acd85a439508
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-03-11 02:59:07 -0500

    frontend placement done

--- script.js
+++ script.js
@@ -9,7 +9,28 @@
 
 
 // find selected answer
-  
+// create a countdown clock
+// Set a valid end date.
+const unixTimeZero = Date.parse('00:010:00');
+const javaScriptRelease = Date.parse('04 Dec 1995 00:12:00 GMT');
+
+console.log(unixTimeZero);
+// expected output: 0
+
+console.log(javaScriptRelease);
+// expected output: 818035920000
+
+// Calculate the time remaining.
+// Convert the time to a usable format.
+// Output the clock data as a reusable object.
+// Display the clock on the page, and stop the clock when it reaches zero.
+
+
+
+
+
+
+
 // if answer is correct, gain time on timer
    
 // Bonus: color the correct answers green
